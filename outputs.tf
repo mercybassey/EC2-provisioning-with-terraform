318aa82c010f345ee02c7f0d579854ac4900f061
mercybassey <udohmercy911@gmail.com>
2026-01-05 13:57:09 +0100

    created outputs in outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+# Output EC2 Instance ID
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.mysql_ec2_instance.id
+}
+
+# Output Private IP of EC2
+output "private_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = aws_instance.mysql_ec2_instance.private_ip
+}
+
+# Output SSM Session Connection Command
+output "ssm_session_command" {
+  description = "Command to connect to the instance using AWS SSM Session Manager"
+  value = "aws ssm start-session --target ${aws_instance.mysql_ec2_instance.id}"
+}
+
+# Output Internet Access State
+output "internet_access_enabled" {
+  description = "Whether outbound internet access is currently enabled for the private subnet"
+  value       = var.enable_internet_access
+}
+
+
