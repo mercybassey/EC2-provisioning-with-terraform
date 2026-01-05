aeb0275a3b02b545f6f3c95d877d11deb8894726
mercybassey <udohmercy911@gmail.com>
2026-01-05 16:16:45 +0100

    modified ami filter for ubuntu 24.04

--- main.tf
+++ main.tf
@@ -4,7 +4,17 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-24.04-amd64-server-*"]
+    values = ["ubuntu/images/*/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 
   filter {
