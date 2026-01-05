47d0a73b1aeea584c241fdde83d5e9a9f004c22b
mercybassey <udohmercy911@gmail.com>
2026-01-05 12:22:28 +0100

    created system session manager for VPC endpoint

--- main.tf
+++ main.tf
@@ -1,96 +1,116 @@
 data "aws_ami" "ubuntu" {
-    most_recent      = true
-    owners           = ["099720109477"]
+  most_recent = true
+  owners      = ["099720109477"]
 
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-*-24.04-amd64-server-*"]
-    } 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-24.04-amd64-server-*"]
+  }
 
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 
 
 data "aws_iam_policy_document" "ec2_policy_document" {
-    statement {
-        effect = "Allow"
+  statement {
+    effect = "Allow"
 
-        principals {
-            type = "Service"
-            identifiers = ["ec2.amazonaws.com"]
-        }
-
-        actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
     }
+
+    actions = ["sts:AssumeRole"]
+  }
 }
 
 locals {
-    common_tags = {
-        Project = "MySQL-Terraform"
-        Environment = "Production"
-        ManagedBy = "Terraform"
-    }
+  common_tags = {
+    Project     = "MySQL-Terraform"
+    Environment = "Production"
+    ManagedBy   = "Terraform"
+  }
 
-    instance_tags = merge(local.common_tags, {
-        Name = var.instance_name
-    })
-  
-    security_group_tags = merge(local.common_tags, {
-        Name = var.security_group
-    })
-    
-    iam_role_tags = merge(local.common_tags, {
-        Name = var.iam_role
-    })
+  instance_tags = merge(local.common_tags, {
+    Name = var.instance_name
+  })
 
-    vpc_tags = merge(local.common_tags, {
-        Name = "mysql-production-vpc"
-    })
+  security_group_tags = merge(local.common_tags, {
+    Name = "ec2-security group"
+  })
 
-    igw_tags = merge(local.common_tags, {
-        Name = var.internet_gateway
-    })
+  iam_role_tags = merge(local.common_tags, {
+    Name = "ec2-iam-role"
+  })
+
+  vpc_tags = merge(local.common_tags, {
+    Name = "mysql-production-vpc"
+  })
+
+  igw_tags = merge(local.common_tags, {
+    Name = "internet-gateway"
+  })
+
+  private_subnet_tags = merge(local.common_tags, {
+    Name = var.mysql_private_subnet
+    Tier = "Private"
+  })
+
+  private_route_table_tags = merge(local.common_tags, {
+    Name = "mysql-private-subnet-route-table"
+    Tier = "Private"
+  })
+
+  vpc_endpoint_sg_tags = merge(local.common_tags, {
+    Name    = " vpc-endpoint-security-group"
+    Purpose = "Allow private subnet to reach VPC endpoints"
+  })
+
+  ssm_endpoint_tags = merge(local.common_tags, {
+    Name    = "ssm-endpoint"
+    Service = "Systems Manager"
+  })
 }
 
 # Create a Custom VPC
 resource "aws_vpc" "ec2_vpc" {
-  cidr_block = var.vpc_cidr
-  tags = local.vpc_tags
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  tags                 = local.vpc_tags
+  enable_dns_support   = true
   enable_dns_hostnames = true
 }
 
 # Create a Security Group
 resource "aws_security_group" "allow_tls" {
-  name = var.security_group
+  name   = "EC2 Security Group"
   vpc_id = aws_vpc.ec2_vpc.id
-  tags = local.security_group_tags
+  tags   = local.security_group_tags
 }
 
 # Create Egress Rule
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-    security_group_id = aws_security_group.allow_tls.id
-    cidr_ipv4 = "0.0.0.0/0"
-    ip_protocol = "-1"
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-    security_group_id = aws_security_group.allow_tls.id
-    cidr_ipv6 = "::/0"
-    ip_protocol = "-1"
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1"
 }
 
 # Create EC2 Instance
 resource "aws_instance" "mysql_ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type           = var.instance_type
-  subnet_id               = local.sorted_subnet_ids[0]
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  iam_instance_profile    = aws_iam_instance_profile.ec2_ssm_instance_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_instance_profile.name
 
   depends_on = [
     aws_iam_role_policy_attachment.ssm
@@ -107,31 +127,91 @@ resource "aws_instance" "mysql_ec2_instance" {
 
 # Create IAM Role for EC2
 resource "aws_iam_role" "ec2_ssm_role" {
-    name = var.iam_role
+  name = "ec2-iam-role"
 
-    assume_role_policy = data.aws_iam_policy_document.ec2_policy_document.json
+  assume_role_policy = data.aws_iam_policy_document.ec2_policy_document.json
 
-    tags = local.iam_role_tags
+  tags = local.iam_role_tags
 }
 
 resource "aws_iam_role_policy_attachment" "ssm" {
-  role = aws_iam_role.ec2_ssm_role.name
+  role       = aws_iam_role.ec2_ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 # Create Instance Profile
 resource "aws_iam_instance_profile" "ec2_ssm_instance_profile" {
-    name = "ec2_ssm_instance_profile"
-    role = aws_iam_role.ec2_ssm_role.name
+  name = "ec2_ssm_instance_profile"
+  role = aws_iam_role.ec2_ssm_role.name
 
-    depends_on = [ 
-        aws_iam_role_policy_attachment.ssm
-    ]
+  depends_on = [
+    aws_iam_role_policy_attachment.ssm
+  ]
 }
 
 # Create Internet Gateway
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "mysql_igw" {
   vpc_id = aws_vpc.ec2_vpc.id
 
   tags = local.igw_tags
 }
+
+# Create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.ec2_vpc.id
+  cidr_block              = var.mysql_private_subnet
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = false
+
+  tags = local.private_subnet_tags
+}
+
+# Create Route Table
+resource "aws_route_table" "mysql_route_table" {
+  vpc_id = aws_vpc.ec2_vpc.id
+  tags   = local.private_route_table_tags
+}
+
+# Create Route Table Association
+resource "aws_route_table_association" "mysql_route_table_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.mysql_route_table.id
+}
+
+# Create a Route
+resource "aws_route" "mysql_route" {
+  count                  = var.enable_internet_access ? 1 : 0
+  route_table_id         = aws_route_table.mysql_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mysql_igw.id
+}
+
+# Create a security group for VPC endpoint
+resource "aws_security_group" "vpc_endpoints_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.ec2_vpc.id
+
+  tags = local.vpc_endpoint_sg_tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.vpc_endpoints_sg.id
+  cidr_ipv4         = var.mysql_private_subnet
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+# Create SSM VPC Endpoint
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.ec2_vpc.id
+  service_name        = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.private_subnet.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+
+  tags = local.ssm_endpoint_tags
+}
+
--- variables.tf
+++ variables.tf
@@ -1,58 +1,44 @@
 variable "region" {
-    type = string
-    description = "AWS Region"
-    default = "af-south-1"
+  type        = string
+  description = "AWS Region"
+  default     = "af-south-1"
 }
 
 variable "instance_name" {
-    type = string
-    description = "EC2 MySQL Instance"
-    default = "My MySQL EC2 Instance"
+  type        = string
+  description = "EC2 MySQL Instance"
+  default     = "My MySQL EC2 Instance"
 }
 
 variable "instance_type" {
-    type = string
-    description = "EC2 Instance Type"
-    default = "t3.micro"
+  type        = string
+  description = "EC2 Instance Type"
+  default     = "t3.micro"
 }
 
 variable "vpc_cidr" {
-    type = string
-    description = "IP range for custom VPC"
-    default = "10.0.0.0/16"
+  type        = string
+  description = "IP range for custom VPC"
+  default     = "10.0.0.0/16"
 }
 
-variable "private_subnet_cidr" {
-  type = string
+variable "mysql_private_subnet" {
+  type        = string
   description = "Private subnet IP range"
-  default = "10.0.10.0/24"
-}
-
-variable "public_subnet_cidr" {
-  type = string
-  description = "Public subnet IP range"
-  default = "10.0.1.0/24"
+  default     = "10.0.10.0/24"
 }
 
 variable "availability_zone" {
-  type = string
+  type        = string
   description = "Availability zone for VPC"
-  default = "af-south-1b"
+  default     = "af-south-1b"
 }
 
-variable "security_group" {
-    type = string
-    description = "Security group with only egress rule"
-    default = "MYSQL Security Group"
+variable "enable_internet_access" {
+  type        = bool
+  description = "Temporarily enable internet access for updates"
+  default     = false
 }
 
-variable "iam_role" {
-    type = string
-    description = "IAM role for EC2 SSM"
-    default = "ec2_ssm_role"
-}
 
-variable "internet_gateway" {
-  type = string
-  description = "Internet gateway for MySQL"
-}
+
